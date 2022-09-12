b319a8b36598b167baf338da6f0d5cfdbbd9ce15
Fedya <apollodor.ash@gmail.com>
2022-09-13 00:22:05 +0300

    Code style: deleted duplicate index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const TsCheckerPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TsCheckerPlugin = require("fork-ts-checker-webpack-plugin")
 
 // Compile directory
 const buildPath = path.resolve(__dirname, "dist");
@@ -11,84 +11,92 @@ const srcPath = path.resolve(__dirname, "src");
 const isProd = process.env.NODE_ENV === "production";
 
 const getSettingsForStyles = (withModules = false) => {
-    return [isProd ? MiniCssExtractPlugin.loader : "style-loader", !withModules ? "css-loader": {
-        loader: "css-loader",
-        options: {
+  return [
+    isProd ? MiniCssExtractPlugin.loader : "style-loader",
+    !withModules
+      ? "css-loader"
+      : {
+          loader: "css-loader",
+          options: {
             modules: {
-                localIdentName: !isProd ?
-                "[path][name]__[local]" : "[hash:base64]"
-            }
-        }
-    }, {
-        loader: "postcss-loader",
-        options: {
-            postcssOptions: {
-                plugins: ["autoprefixer"]
-            }
-        }
-    }, "sass-loader"]
-}
+              localIdentName: !isProd
+                ? "[path][name]__[local]"
+                : "[hash:base64]",
+            },
+          },
+        },
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: ["autoprefixer"],
+        },
+      },
+    },
+    "sass-loader",
+  ];
+};
 
 module.exports = {
-    entry: path.join(srcPath, "index.tsx"),
-    target: !isProd ? "web" : "browserslist",
-    devtool: isProd ? "hidden-source-map" : "eval-source-map",
-    output: {
-        path: buildPath,
-        filename: "bundle.js"
+  entry: path.join(srcPath, "index.tsx"),
+  target: !isProd ? "web" : "browserslist",
+  devtool: isProd ? "hidden-source-map" : "eval-source-map",
+  output: {
+    path: buildPath,
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join("public", "index.html"),
+    }),
+    !isProd && new ReactRefreshWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name]-[hash].css",
+    }),
+    new TsCheckerPlugin(),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/, //.ts/.js/.tsx/.jsx ?
+        use: "babel-loader",
+      },
+      {
+        test: /\.module\.s?css$/,
+        use: getSettingsForStyles(true),
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /\.module\.s?css$/,
+        use: getSettingsForStyles(),
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      // E.g. if file starts with "components" then webpack will search for it in this directory
+      components: path.join(srcPath, "components"),
+      config: path.join(srcPath, "config"),
+      stores: path.join(srcPath, "stores"),
+      assets: path.join(srcPath, "assets"),
+      pages: path.join(srcPath, "pages"),
+      hooks: path.join(srcPath, "hooks"),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(srcPath, "index.html")
-        }),
-        !isProd && new ReactRefreshWebpackPlugin(), new MiniCssExtractPlugin(
-            {
-                filename: "[name]-[hash].css"
-            }
-        ), new TsCheckerPlugin()
-    ].filter(Boolean),
-    module: {
-        rules: [
-            {
-            test: /\.[tj]sx?$/, //.ts/.js/.tsx/.jsx ?
-            use: "babel-loader"
-            },
-            {
-                test: /\.module\.s?css$/,
-                use: getSettingsForStyles(true)
-            },
-            {
-                test: /\.s?css$/,
-                exclude: /\.module\.s?css$/,
-                use: getSettingsForStyles()
-            },
-            {
-                test: /\.(png|svg|jpg)$/,
-                type: "asset",
-                parser: {
-                    dataUrlCondition: {
-                        maxSize: 10 * 1024
-                    }
-                }
-            }
-    ]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-        alias: {
-            // E.g. if file starts with "components" then webpack will search for it in this directory
-            components: path.join(srcPath, "components"),
-            config: path.join(srcPath, "config"),
-            stores: path.join(srcPath, "stores"),
-            assets: path.join(srcPath, "assets"),
-            pages: path.join(srcPath, "pages"),
-            hooks: path.join(srcPath, "hooks"),
-        }
-    },
-    devServer: {
-        host: "127.0.0.1",
-        port: 9000,
-        hot: true,
-        historyApiFallback: true
-    }
-}
+  },
+  devServer: {
+    host: "127.0.0.1",
+    port: 9000,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
